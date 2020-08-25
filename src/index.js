c26d8cf046e6afff7e5d157b4abbd915509322e8
bolex222 <biblex1222@gmail.com>
2020-08-26 01:16:37 +0200

    add firebase && clean project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import * as firebase from 'firebase'
+import { firebaseConfig } from './config/config'
+
+function App () {
+  const [expressions, setExpression] = useState([])
+
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig)
+    const ref = firebase.database().ref('expression')
+    ref.on('value', snapshot => {
+      setExpression(snapshot.val())
+    })
+  }, [])
+
+  return (
+    <>
+      <h1>Welcome on Weird Expression</h1>
+      {expressions.map((expression, index) => <div key={index}>{index} : {expression}</div>)}
+    </>
+  )
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+

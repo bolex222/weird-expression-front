5f76a573f447a500080a47908fa09feeead4fcec
basile <biblex1222@gmail.com>
2021-01-13 22:57:28 +0100

    add sample firebase onfig

---
+++ src/config/sample.config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "sample-url.firebaseapp.com",
+    databaseURL: "https://sample.firebaseio.com",
+    projectId: "sample",
+    storageBucket: "sample.appspot.com",
+    messagingSenderId: "sample",
+    appId: "sample",
+    measurementId: "sample"
+}
+
+
+export {firebaseConfig}
